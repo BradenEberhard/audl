0a04980967e335a79f3c74fdc21f5e2d9b90025c
BradenEberhard <50636857+BradenEberhard@users.noreply.github.com>
2023-07-05 13:23:42 -0600

    player and team endpoints

---
+++ audl/stats/endpoints/games.py
@@ -0,0 +1,16 @@
+from audl.stats.endpoints._base import Endpoint
+import requests
+import pandas as pd
+
+class Games(Endpoint):
+    def __init__(self):
+        super().__init__("https://www.backend.audlstats.com/api/v1/games")
+
+    def get_request(self, endpoint):
+        self.current_request = requests.get(f'{self.base_url}{endpoint}').json()['data']
+    
+    def set_games(self, years='all'):
+        self.current_request = requests.get(f'{self.base_url}?years={years}').json()
+    
+    def get_games(self, years='all'):
+        return pd.DataFrame(requests.get(f'{self.base_url}?years={years}').json()['data'])
